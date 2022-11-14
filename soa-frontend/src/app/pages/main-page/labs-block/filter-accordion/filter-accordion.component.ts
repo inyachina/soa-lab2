22f2d9fcbaf7a04012b1044e55261c1b8bf6ec65
inyachina <dianainyachina@gmail.com>
2022-11-14 13:48:52 +0300

    before Egor penetration testing

--- soa-frontend/src/app/pages/main-page/labs-block/filter-accordion/filter-accordion.component.ts
+++ soa-frontend/src/app/pages/main-page/labs-block/filter-accordion/filter-accordion.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
 import {MatAccordion} from "@angular/material/expansion";
+import {FormBuilder} from "@angular/forms";
 
 @Component({
   selector: 'app-filter-accordion',
@@ -9,48 +10,62 @@ import {MatAccordion} from "@angular/material/expansion";
 export class FilterAccordionComponent implements OnInit {
   @ViewChild(MatAccordion) accordion!: MatAccordion;
 
+  public filterForm = this._fb.group(
+    {
+      name: this._fb.group(
+        {
+          sort: [null],
+          filter: [null]
+        }
+      )
+    }
+  )
   public properties = [
     {
       name: "Name",
-      server_name: "name"
+      formGroup: this.filterForm.controls.name
     },
-    {
-      name: "Creation Date",
-      server_name: "creation_date"
-    },
-    {
-      name: "Personal Qualities Maximum",
-      server_name: "personalQualitiesMaximum"
-    },
-    {
-      name: "Difficulty",
-      server_name: "difficulty"
-    }
+    // {
+    //   name: "Creation Date",
+    //   server_name: "creation_date"
+    // },
+    // {
+    //   name: "Personal Qualities Maximum",
+    //   server_name: "personalQualitiesMaximum"
+    // },
+    // {
+    //   name: "Difficulty",
+    //   server_name: "difficulty"
+    // }
   ]
 
-  public coordinatesProperties = [{
-    name: "x",
-    server_name: "x"
-  },
-    {
-      name: "y",
-      server_name: "y"
-    },
+  public coordinatesProperties = [
+  //   {
+  //   name: "x",
+  //   server_name: "x"
+  // },
+  //   {
+  //     name: "y",
+  //     server_name: "y"
+  //   },
   ];
-  public disciplineProperties = [{
-    name: "name",
-    server_name: "name"
-  },
-    {
-      name: "Lecture Hours",
-      server_name: "lecture_hours"
-    },{
-      name: "Self Study Hours",
-      server_name: "self_study_hours"
-    },
+  public disciplineProperties = [
+  //   {
+  //   name: "name",
+  //   server_name: "name"
+  // },
+  //   {
+  //     name: "Lecture Hours",
+  //     server_name: "lecture_hours"
+  //   },{
+  //     name: "Self Study Hours",
+  //     server_name: "self_study_hours"
+  //   },
   ]
 
-  constructor() {
+  constructor(
+    private _fb: FormBuilder
+  ) {
   }
 
   ngOnInit(): void {
--- soa-frontend/src/app/pages/main-page/labs-block/filter-accordion/filter-rule/filter-rule.component.ts
+++ soa-frontend/src/app/pages/main-page/labs-block/filter-accordion/filter-rule/filter-rule.component.ts
@@ -1,4 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
+import {FormControl, FormGroup} from "@angular/forms";
+import {MatRadioChange} from "@angular/material/radio";
 
 @Component({
   selector: 'app-filter-rule',
@@ -8,11 +10,27 @@ import {Component, Input, OnInit} from '@angular/core';
 export class FilterRuleComponent implements OnInit {
 
   @Input("property")
-  public property!: any;
-
-  constructor() { }
-
-  ngOnInit(): void {
+  public property!: {
+    name: string,
+    formGroup: FormGroup
   }
 
+  public filterControl!: FormControl;
+  public sortControl!: FormControl;
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+    // @ts-ignore
+    this.sortControl = this.property.formGroup.controls.sort;
+    // @ts-ignore
+    this.filterControl = this.property.formGroup.controls.filter;
+    console.log(this.sortControl)
+
+  }
+
+  onChangeSort(event: MatRadioChange) {
+    this.sortControl.setValue(event.value)
+  }
 }
